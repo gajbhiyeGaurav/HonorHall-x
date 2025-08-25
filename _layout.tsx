c12fec9e6158eacc01ef0525a884e86ca68be92c
gajbhiyeGaurav <gajbhiyegaurav409@gmail.com>
2025-08-25 20:05:20 +0530

    Add files via upload

---
+++ _layout.tsx
@@ -0,0 +1,7 @@
+import { Redirect } from "expo-router";
+import { Stack } from 'expo-router';
+import '../../global.css';
+
+export default function RootLayout() {
+    return <Stack screenOptions={{ headerShown: false }} />;
+}
